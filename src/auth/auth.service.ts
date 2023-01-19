2cb4d8a75100e67acd62b818e8c3c2f8e8ef787e
rinku sutradhar <sopnonill87@gmail.com>
2023-01-19 17:15:16 +0600

    auth guard v1 added for protected route

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,11 +4,13 @@ import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon2 from "argon2";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
+import { JwtService } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable({})
 
 export class AuthService {
-    constructor(private prisma: PrismaService){
+    constructor(private prisma: PrismaService, private jwt: JwtService, private config: ConfigService){
         
     }
 
@@ -25,9 +27,13 @@ export class AuthService {
                 }
             });
     
-            delete user.hash;
+            //delete user.hash;
             //return saved user
-            return user;
+            //return user;
+
+            //after implementing jwt token, no need to return user obj
+
+            return this.signToken(user.id, user.email);
         } catch (error) {
             if(error instanceof PrismaClientKnownRequestError){
                 if(error.code === 'P2002'){
@@ -60,7 +66,29 @@ export class AuthService {
             throw new ForbiddenException('Credentials incorrect');
         }
 
-        delete user.hash;
-        return user;
+        // delete user.hash;
+        // return user;
+        //after implementing jwt token, no need to return user obj
+
+        return this.signToken(user.id, user.email);
+    }
+
+    async signToken(userId: number, email: string): Promise<{ access_token: string }>{
+        const payload = {
+            sub: userId,
+            email: email
+        };
+
+        const secret = this.config.get('JWT_SECRET');
+
+        const token = await this.jwt.signAsync(payload, {
+            expiresIn: "15m",
+            secret: secret
+        });
+
+        return {
+            access_token: token,
+        }
+
     }
 }
